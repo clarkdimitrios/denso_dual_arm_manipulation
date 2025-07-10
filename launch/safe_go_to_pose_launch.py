c63d8a05edc833356833bb7081f7c1a10a696baf
Clark <cabourjeily3@gatech.edu>
2025-07-10 15:21:23 -0400

    w/ safety launch file

---
+++ launch/safe_go_to_pose_launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='manip_facts_lab',
+            executable='go_to_pose_node',
+            name='go_to_pose_node',
+        ),
+        Node(
+            package='manip_facts_lab',
+            executable='add_virtual_walls',
+            name='add_virtual_walls',
+        ),
+    ])

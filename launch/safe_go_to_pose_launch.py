bead0277f1bed9bac960d33282e81ed06c326fc6
Clark <cabourjeily3@gatech.edu>
2025-07-16 18:39:06 -0400

    launch namespaces

--- launch/safe_go_to_pose_launch.py
+++ launch/safe_go_to_pose_launch.py
@@ -1,16 +1,41 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, LogInfo, Shutdown
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+
+
+def check_namespace_prefix(context, *args, **kwargs):
+    namespace_prefix = LaunchConfiguration('namespace').perform(context)
+    actions = []
+    if not namespace_prefix:
+        actions += [
+            LogInfo(msg="\n\n[WARN] 'namespace' is empty — Using default joint names without prefix."),
+            LogInfo(msg="If using multiple arms, you probably want to set this as 'left_' or 'right_'.")
+        ]
+    return actions
+
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'namespace',
+            default_value='',
+            description='Prefix for joint names (e.g., left_ or right_)'
+        ),
+
+        OpaqueFunction(function=check_namespace_prefix),
+
         Node(
             package='manip_facts_lab',
             executable='add_virtual_walls',
             name='add_virtual_walls',
+            parameters=[{'namespace': LaunchConfiguration('namespace')}]
         ),
+
         Node(
             package='manip_facts_lab',
             executable='go_to_pose_node',
             name='go_to_pose_node',
+            parameters=[{'namespace': LaunchConfiguration('namespace')}]
         ),
     ])
--- launch/safe_waypoints_launch.py
+++ launch/safe_waypoints_launch.py
@@ -4,15 +4,26 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
-def check_csv_filename(context, *args, **kwargs):
+def check_required_arguments(context, *args, **kwargs):
     csv_filename = LaunchConfiguration('csv_filename').perform(context)
+    namespace_prefix = LaunchConfiguration('namespace').perform(context)
+
+    actions = []
+
     if not csv_filename:
-        return [
+        actions += [
             LogInfo(msg="\n\n[ERROR] Missing required launch argument 'csv_filename'."),
-            LogInfo(msg="Example Usage: ros2 launch manip_facts_lab safe_waypoints_launch.py csv_filename:=waypoints_0.csv"),
+            LogInfo(msg="Example usage: ros2 launch manip_facts_lab safe_waypoints_launch.py csv_filename:=waypoints.csv namespace:=left_"),
             Shutdown()
         ]
-    return []
+
+    if not namespace_prefix:
+        actions += [
+            LogInfo(msg="\n\n[WARN] 'namespace' is empty — Using default joint names without prefix."),
+            LogInfo(msg="If using multiple arms, you probably want to set this as 'left_' or 'right_'.")
+        ]
+
+    return actions
 
 
 def generate_launch_description():
@@ -22,19 +33,28 @@ def generate_launch_description():
             default_value='',
             description='Path to the CSV file with waypoints'
         ),
+        DeclareLaunchArgument(
+            'namespace',
+            default_value='',
+            description='Prefix for joint names (e.g., left_ or right_)'
+        ),
 
-        OpaqueFunction(function=check_csv_filename),
+        OpaqueFunction(function=check_required_arguments),
 
         Node(
             package='manip_facts_lab',
             executable='add_virtual_walls',
             name='add_virtual_walls',
+            parameters=[{'namespace': LaunchConfiguration('namespace')}]
         ),
 
         Node(
             package='manip_facts_lab',
             executable='waypoints_node',
             name='waypoints_node',
-            parameters=[{'csv_filename': LaunchConfiguration('csv_filename')}],
+            parameters=[
+                {'csv_filename': LaunchConfiguration('csv_filename')},
+                {'namespace': LaunchConfiguration('namespace')}
+            ],
         ),
     ])
